88f52cc184cd795c5070b665c6f93f9cc7c0b29f
Kazi Md. Saidul <kazimdsaidul@gmail.com>
2021-07-12 20:05:12 +0600

    initiation project

---
+++ app/src/main/java/com/saidul/dogbreed_changers/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.saidul.dogbreed_changers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
